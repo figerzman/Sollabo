4655497ae7f337d81a9b5f7c00862dfc03ae10a5
figerzman <figerzman@naver.com>
2022-10-24 09:22:34 +0900

    게시판 기본 틀

--- src/main/java/com/web/sollabo/board/controller/BoardController.java
+++ src/main/java/com/web/sollabo/board/controller/BoardController.java
@@ -1,5 +1,18 @@
 package com.web.sollabo.board.controller;
 
-public class BoardController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping("board")
+public class BoardController {
+	
+	
+	
+	@RequestMapping(value = "baseBoard" ,method =RequestMethod.GET)
+	public String baseBoard() {
+		String url = "board/baseBoard";
+		return url;
+	}
 }
