9602eee7eca5e27cb33faa73de9ac6f4cd4211a0
geunho <2339lkh@gmial.com>
2022-10-27 10:52:01 +0900

    주석처리한 상태

--- src/main/java/com/web/sollabo/cart/service/CartService.java
+++ src/main/java/com/web/sollabo/cart/service/CartService.java
@@ -1,13 +1,13 @@
-package com.web.sollabo.cart.service;
-
-import com.web.sollabo.cart.dto.CartDTO;
-
-public interface CartService {
-	
-	
-	/* 장바구니 추가 */
-	private int addCart(CartDTO cart);
-	
-	
-	
-}
+//package com.web.sollabo.cart.service;
+//
+//import com.web.sollabo.cart.dto.CartDTO;
+//
+//public interface CartService {
+//	
+//	
+//	/* 장바구니 추가 */
+//	private int addCart(CartDTO cart);
+//	
+//	
+//	
+//}
--- src/main/java/com/web/sollabo/mybatis/cart/CartMapper.java
+++ src/main/java/com/web/sollabo/mybatis/cart/CartMapper.java
@@ -2,22 +2,22 @@ package com.web.sollabo.mybatis.cart;
 
 import com.web.sollabo.cart.dto.CartDTO;
 
-public class CartMapper {
-	
-	/* 카트 추가 */
-	public int addCart(CartDTO cart);
-	
-	/* 카트 삭제 */
-	public int deleteCart(int cartNO);
-	
-	/* 카트 수량 수정 */
-	public int modifyCount(CartDTO cart);
-	
-	/* 카트 목록 */
-	public List<CartDTO> getCart(String mem_no);	
-	
-	/* 카트 확인 */
-	public CartDTO checkCart(CartDTO cart);
-	
-	
-}
+//public class CartMapper {
+//	
+//	/* 카트 추가 */
+//	public int addCart(CartDTO cart);
+//	
+//	/* 카트 삭제 */
+//	public int deleteCart(int cartNO);
+//	
+//	/* 카트 수량 수정 */
+//	public int modifyCount(CartDTO cart);
+//	
+//	/* 카트 목록 */
+//	public List<CartDTO> getCart(String mem_no);	
+//	
+//	/* 카트 확인 */
+//	public CartDTO checkCart(CartDTO cart);
+//	
+//	
+//}
