97b61259e27d688f70a23cd1c7db6494af19d2df
figerzman <figerzman@naver.com>
2022-11-03 11:00:02 +0900

    1212

--- src/main/java/com/web/sollabo/HomeController.java
+++ src/main/java/com/web/sollabo/HomeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Handles requests for the application home page. test1
+ * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
